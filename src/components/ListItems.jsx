c1d2015fc7018c51528f94adbbb77715e5a91632
Jose Germán Martínez <jgermanmm@gmail.com>
2025-02-12 00:58:32 -0300

    Update ListItems.jsx

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -9,7 +9,7 @@ const ListItems = () => {
             {
                 links && links.map((item) => (
                     <div class="w-11/12 max-w-md bg-gray-800 border-2 border-blue-500 rounded-2xl p-4 mb-4 hover:bg-gray-700 flex items-center" key={item.id}>
-                        <i class="fa-regular fa-newspaper text-3xl mr-4 text-gray-400"></i>
+                        <i class=`{item.faIcon} text-3xl mr-4 text-gray-400`></i>
                         <div>
                             <h3 class="text-xl font-bold">{item.titulo}</h3>
                             <p class="text-gray-300"><a href={item.link}>{item.linkText}</a>{item.descripcion}</p>
